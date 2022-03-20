26c77dfd1b2f481a3cf1180ffa96eb90348899e7
Михаил Королев <michael199-fts@yandex.ru>
2022-03-20 14:27:02 +0300

    First task. Created "participants" model

--- api/models.py
+++ api/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+class Participant(models.Model):
+    male = 'ML'
+    female = 'FL'
+    SEX_CHOICES = [
+        (male, 'male'),
+        (female, 'female')
+    ]
+    photo = \
+        models.ImageField(upload_to='photos/')
+    first_name = \
+        models.CharField(max_length=30)
+    last_name = \
+        models.CharField(max_length=30)
+    email = \
+        models.EmailField()
+    sex = \
+        models.CharField(max_length=2, choices=SEX_CHOICES)
